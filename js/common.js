e893852720fc38cb1f9f1b9df7a2a153fea3218e
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-19 14:51:05 +0300

    modulesExportRequireLearned

---
+++ js/common.js
@@ -0,0 +1,28 @@
+// Оформите этот код в виде модуля CommonJS. Экспортируйте все функции, кроме вспомогательных.
+
+
+function square(num) {
+    return num ** 2;
+}
+function cube(num) {
+    return num ** 3;
+}
+function avg(arr) {
+    return sum(arr, 1) / arr.length;
+}
+function digitsSum(num) {
+    return sum(String(num).split(''));
+}
+
+// вспомогательная функция
+function sum(arr) {
+    let res = 0;
+
+    for (let elem of arr) {
+        res += +elem;
+    }
+
+    return res;
+}
+
+module.exports = { square, cube, avg, digitsSum }
--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-console.log('hello')
+let math = require('./math');
+let res = math.square(3) + math.cube(3);
+console.log(res)
+
+let common = require('./common');
+console.log(`Квадрат числа 5 = ${common.square(5)}, Куб числа 6 = ${common.cube(6)}, Средняя сумма чисел 1, 5, 7  = 
+${common.avg([1, 5, 7])}, Сумма чисел числа 123456789999 = ${common.digitsSum(123456789999)}`)
