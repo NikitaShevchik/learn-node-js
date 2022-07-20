a08282a2aa9801035838bde26b5c8b6ad6aef313
NikitaShevchik <shevchik.nikita@gmail.com>
2022-07-20 14:42:58 +0300

    moduleEs

--- js/index.js
+++ js/index.js
@@ -1,55 +1,65 @@
-let math = require('./math');
-let res = math.square(3) + math.cube(3);
-console.log(res)
+// let math = require('./math');
+// let res = math.square(3) + math.cube(3);
+// console.log(res)
 
-let common = require('./common');
-// console.log(`Квадрат числа 5 = ${common.square(5)}, Куб числа 6 = ${common.cube(6)}, Средняя сумма чисел 1, 5, 7  = 
-// ${common.avg([1, 5, 7])}, Сумма чисел числа 123456789999 = ${common.digitsSum(123456789999)}`)
-
-
-//С помощью модуля из предыдущей задачи найдите сумму квадрата и куба числа 3.
-// Импортируйте для этого только необходимые функции.
-
-let comRes = common.square(3) + common.cube(3);
-console.log(comRes)
+// let common = require('./common');
+// // console.log(`Квадрат числа 5 = ${common.square(5)}, Куб числа 6 = ${common.cube(6)}, Средняя сумма чисел 1, 5, 7  =
+// // ${common.avg([1, 5, 7])}, Сумма чисел числа 123456789999 = ${common.digitsSum(123456789999)}`)
 
 
-let array = require('./arrays');
-console.log(array.arrNum);
-console.log(array.arrNumTwo);
-function arraySum(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum = sum + arr[i];
-    }
-    console.log(sum)
-}
-arraySum(array.arrNum)
+// //С помощью модуля из предыдущей задачи найдите сумму квадрата и куба числа 3.
+// // Импортируйте для этого только необходимые функции.
 
-// Сделайте модуль, экспортирующий одну функцию.
-let oneF = require('./onefunction');
-console.log(oneF.ye(5))
-console.log(oneF.square(5), oneF.cube(10))
-console.log(oneF.squareTwo(11), oneF.cubeTwo(11))
+// let comRes = common.square(3) + common.cube(3);
+// console.log(comRes)
 
 
-let parameter = require('./parameter');
-let data = 'abcde'; // эти данные хотим передать
-let test = parameter(data, 2)
-console.log(test)
+// let array = require('./arrays');
+// console.log(array.arrNum);
+// console.log(array.arrNumTwo);
+// function arraySum(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum = sum + arr[i];
+//     }
+//     console.log(sum)
+// }
+// arraySum(array.arrNum)
 
-// let _ = require('underscore');
-// let arr = [1, 2, 3, 4, 5]
-// let resUn = _.first(arr) + _.last(arr);
+// // Сделайте модуль, экспортирующий одну функцию.
+// let oneF = require('./onefunction');
+// console.log(oneF.ye(5))
+// console.log(oneF.square(5), oneF.cube(10))
+// console.log(oneF.squareTwo(11), oneF.cubeTwo(11))
+
+
+// let parameter = require('./parameter');
+// let data = 'abcde'; // эти данные хотим передать
+// let test = parameter(data, 2)
+// console.log(test)
+
+// // let _ = require('underscore');
+// // let arr = [1, 2, 3, 4, 5]
+// // let resUn = _.first(arr) + _.last(arr);
+// // console.log(resUn);
+
+// let { first, last } = require('underscore');
+// let arr = [11, 2, 3, 4, 55]
+// let resUn = first(arr) + last(arr);
 // console.log(resUn);
 
-let { first, last } = require('underscore');
-let arr = [11, 2, 3, 4, 55]
-let resUn = first(arr) + last(arr);
-console.log(resUn);
+// let lo = require('lodash')
+// let testArr = [1, 2, 2, 3, 4, 5, 6, 2]
+// let resLodash = lo.sortedIndexOf(testArr, 2)
+// console.log(resLodash)
 
-let lo = require('lodash')
-let testArr = [1, 2, 2, 3, 4, 5, 6, 2]
-let resLodash = lo.sortedIndexOf(testArr, 2)
-console.log(resLodash)
+/*--------------------------------------------------ES--------------------------------------------------*/
 
+// import { square, cube } from './math.js';
+
+// let res = square(2) + cube(3);
+// console.log(res);
+
+import { square, cube, avg, digitsSum } from './math.js';
+let res = square(3) + cube(3)
+console.log(res)
--- js/math.js
+++ js/math.js
@@ -1,6 +1,37 @@
-module.exports.square = function (num) {
-    return num * num;
+// module.exports.square = function (num) {
+//     return num * num;
+// }
+// module.exports.cube = function (num) {
+//     return num * num * num;
+// }
+
+// export function square(num) {
+//     return num * num;
+// }
+// export function cube(num) {
+//     return num * num * num;
+// }
+
+export function square(num) {
+    return num ** 2;
 }
-module.exports.cube = function (num) {
-    return num * num * num;
+export function cube(num) {
+    return num ** 3;
+}
+export function avg(arr) {
+    return sum(arr, 1) / arr.length;
+}
+export function digitsSum(num) {
+    return sum(String(num).split(''));
+}
+
+// вспомогательная функция
+function sum(arr) {
+    let res = 0;
+
+    for (let elem of arr) {
+        res += +elem;
+    }
+
+    return res;
 }
